cecff8924dfb12408b24dec355c91e69b8967095
MuMingLin <159245220+MuMingLin@users.noreply.github.com>
2024-03-31 22:21:01 +0800

    Correct the logic issue in determining the most visited wintering region

--- data_prep_Mar2024.R
+++ data_prep_Mar2024.R
@@ -159,8 +159,60 @@ check = BFS_all.UT.2 %>%
 
 # use a longer time scale (1994-2023), and finer seasonal intervals (1 Dec-28 Feb, 1 June-31 Aug) to determine the region in each sampling occasion
 
+# previous methods have a logical problem in the region picking criteria 
+# BFS_06_23_winter = BFS_all.UT.2 %>%
+#   mutate(time = as.Date(observation.date)) %>%
+#   mutate(SO = case_when(
+#     time>=as.Date("1994/12/1")&time<=as.Date("1995/2/28")~"1994W",	time>=as.Date("1995/6/1")&time<=as.Date("1995/8/31")~"1995S",
+#     time>=as.Date("1995/12/1")&time<=as.Date("1996/2/29")~"1995W",	time>=as.Date("1996/6/1")&time<=as.Date("1996/8/31")~"1996S",
+#     time>=as.Date("1996/12/1")&time<=as.Date("1997/2/28")~"1996W",	time>=as.Date("1997/6/1")&time<=as.Date("1997/8/31")~"1997S",
+#     time>=as.Date("1997/12/1")&time<=as.Date("1998/2/28")~"1997W",	time>=as.Date("1998/6/1")&time<=as.Date("1998/8/31")~"1998S",
+#     time>=as.Date("1998/12/1")&time<=as.Date("1999/2/28")~"1998W",	time>=as.Date("1999/6/1")&time<=as.Date("1999/8/31")~"1999S",
+#     time>=as.Date("1999/12/1")&time<=as.Date("2000/2/29")~"1999W",	time>=as.Date("2000/6/1")&time<=as.Date("2000/8/31")~"2000S",
+#     time>=as.Date("2000/12/1")&time<=as.Date("2001/2/28")~"2000W",	time>=as.Date("2001/6/1")&time<=as.Date("2001/8/31")~"2001S",
+#     time>=as.Date("2001/12/1")&time<=as.Date("2002/2/28")~"2001W",	time>=as.Date("2002/6/1")&time<=as.Date("2002/8/31")~"2002S",
+#     time>=as.Date("2002/12/1")&time<=as.Date("2003/2/28")~"2002W",	time>=as.Date("2003/6/1")&time<=as.Date("2003/8/31")~"2003S",
+#     time>=as.Date("2003/12/1")&time<=as.Date("2004/2/29")~"2003W",	time>=as.Date("2004/6/1")&time<=as.Date("2004/8/31")~"2004S",
+#     time>=as.Date("2004/12/1")&time<=as.Date("2005/2/28")~"2004W",	time>=as.Date("2005/6/1")&time<=as.Date("2005/8/31")~"2005S",
+#     time>=as.Date("2005/12/1")&time<=as.Date("2006/2/28")~"2005W",	time>=as.Date("2006/6/1")&time<=as.Date("2006/8/31")~"2006S",
+#     time>=as.Date("2006/12/1")&time<=as.Date("2007/2/28")~"2006W",	time>=as.Date("2007/6/1")&time<=as.Date("2007/8/31")~"2007S",
+#     time>=as.Date("2007/12/1")&time<=as.Date("2008/2/29")~"2007W",	time>=as.Date("2008/6/1")&time<=as.Date("2008/8/31")~"2008S",
+#     time>=as.Date("2008/12/1")&time<=as.Date("2009/2/28")~"2008W",	time>=as.Date("2009/6/1")&time<=as.Date("2009/8/31")~"2009S",
+#     time>=as.Date("2009/12/1")&time<=as.Date("2010/2/28")~"2009W",	time>=as.Date("2010/6/1")&time<=as.Date("2010/8/31")~"2010S",
+#     time>=as.Date("2010/12/1")&time<=as.Date("2011/2/28")~"2010W",	time>=as.Date("2011/6/1")&time<=as.Date("2011/8/31")~"2011S",
+#     time>=as.Date("2011/12/1")&time<=as.Date("2012/2/29")~"2011W",	time>=as.Date("2012/6/1")&time<=as.Date("2012/8/31")~"2012S",
+#     time>=as.Date("2012/12/1")&time<=as.Date("2013/2/28")~"2012W",	time>=as.Date("2013/6/1")&time<=as.Date("2013/8/31")~"2013S",
+#     time>=as.Date("2013/12/1")&time<=as.Date("2014/2/28")~"2013W",	time>=as.Date("2014/6/1")&time<=as.Date("2014/8/31")~"2014S",
+#     time>=as.Date("2014/12/1")&time<=as.Date("2015/2/28")~"2014W",	time>=as.Date("2015/6/1")&time<=as.Date("2015/8/31")~"2015S",
+#     time>=as.Date("2015/12/1")&time<=as.Date("2016/2/29")~"2015W",	time>=as.Date("2016/6/1")&time<=as.Date("2016/8/31")~"2016S",
+#     time>=as.Date("2016/12/1")&time<=as.Date("2017/2/28")~"2016W",	time>=as.Date("2017/6/1")&time<=as.Date("2017/8/31")~"2017S",
+#     time>=as.Date("2017/12/1")&time<=as.Date("2018/2/28")~"2017W",	time>=as.Date("2018/6/1")&time<=as.Date("2018/8/31")~"2018S",
+#     time>=as.Date("2018/12/1")&time<=as.Date("2019/2/28")~"2018W",	time>=as.Date("2019/6/1")&time<=as.Date("2019/8/31")~"2019S",
+#     time>=as.Date("2019/12/1")&time<=as.Date("2020/2/29")~"2019W",	time>=as.Date("2020/6/1")&time<=as.Date("2020/8/31")~"2020S",
+#     time>=as.Date("2020/12/1")&time<=as.Date("2021/2/28")~"2020W",	time>=as.Date("2021/6/1")&time<=as.Date("2021/8/31")~"2021S",
+#     time>=as.Date("2021/12/1")&time<=as.Date("2022/2/28")~"2021W",	time>=as.Date("2022/6/1")&time<=as.Date("2022/8/31")~"2022S",
+#     time>=as.Date("2022/12/1")&time<=as.Date("2023/2/28")~"2022W",	time>=as.Date("2023/6/1")&time<=as.Date("2023/8/31")~"2023S"
+#   )) %>%
+#   group_by(band.of.ind., SO, ) %>%
+#   summarize(region_4 = list(region_4)) %>%
+#   mutate(region_4 = map(region_4, ~{
+#     freq = table(.x)
+#     max_freq = max(freq)
+#     # if same frequencies -> keep the latest record
+#     if (sum(freq == max_freq) > 1) {
+#       head(names(freq)[freq == max_freq], 1)
+#     } else {
+#       names(freq)[which.max(freq)]
+#     }
+#   })) %>%
+#   arrange(SO) %>%  # add this line to sort by period
+#   pivot_wider(names_from = SO, values_from = region_4, values_fill = list("-")) # fill the empty fields with "-"
+
+
 BFS_06_23_winter = BFS_all.UT.2 %>%
-  mutate(time = as.Date(observation.date)) %>%
+  # Convert observation.date to Date format
+  mutate(time = as.Date(observation.date),
+         region_4 = as.character(region_4)) %>%  # Convert region_4 to character
   mutate(SO = case_when(
     time>=as.Date("1994/12/1")&time<=as.Date("1995/2/28")~"1994W",	time>=as.Date("1995/6/1")&time<=as.Date("1995/8/31")~"1995S",
     time>=as.Date("1995/12/1")&time<=as.Date("1996/2/29")~"1995W",	time>=as.Date("1996/6/1")&time<=as.Date("1996/8/31")~"1996S",
@@ -190,22 +242,150 @@ BFS_06_23_winter = BFS_all.UT.2 %>%
     time>=as.Date("2019/12/1")&time<=as.Date("2020/2/29")~"2019W",	time>=as.Date("2020/6/1")&time<=as.Date("2020/8/31")~"2020S",
     time>=as.Date("2020/12/1")&time<=as.Date("2021/2/28")~"2020W",	time>=as.Date("2021/6/1")&time<=as.Date("2021/8/31")~"2021S",
     time>=as.Date("2021/12/1")&time<=as.Date("2022/2/28")~"2021W",	time>=as.Date("2022/6/1")&time<=as.Date("2022/8/31")~"2022S",
-    time>=as.Date("2022/12/1")&time<=as.Date("2023/2/28")~"2022W",	time>=as.Date("2023/6/1")&time<=as.Date("2023/8/31")~"2023S"
+    time>=as.Date("2022/12/1")&time<=as.Date("2023/2/28")~"2022W",	time>=as.Date("2023/6/1")&time<=as.Date("2023/8/31")~"2023S",
+    time>=as.Date("2023/12/1")&time<=as.Date("2024/2/28")~"2023W",
+    TRUE ~ NA_character_
   )) %>%
-  group_by(band.of.ind., SO, ) %>%
-  summarize(region_4 = list(region_4)) %>%
-  mutate(region_4 = map(region_4, ~{
-    freq = table(.x)
-    max_freq = max(freq)
-    # if same frequencies -> keep the latest record
-    if (sum(freq == max_freq) > 1) {
-      head(names(freq)[freq == max_freq], 1)
+  group_by(band.of.ind., SO) %>%
+  summarize(
+    regions = list(region_4),
+    dates = list(time),
+    .groups = 'drop'
+  ) %>%
+  mutate(region_4 = map2(regions, dates, ~{
+    region_list <- .x
+    date_list <- .y
+    freq <- table(region_list)
+    max_freq <- max(freq)
+    tied_regions <- names(freq[freq == max_freq])
+    
+    if (length(tied_regions) > 1) {
+      tied_dates <- date_list[region_list %in% tied_regions]
+      latest_date <- max(tied_dates)
+      latest_region_indices <- which(region_list %in% tied_regions & date_list == latest_date)
+      latest_region <- region_list[latest_region_indices[1]]
+      
+      if (length(unique(date_list[latest_region_indices])) > 1) {
+        return("Check")
+      } else {
+        return(latest_region)
+      }
     } else {
-      names(freq)[which.max(freq)]
+      return(tied_regions)
     }
   })) %>%
-  arrange(SO) %>%  # add this line to sort by period
-  pivot_wider(names_from = SO, values_from = region_4, values_fill = list("-")) # fill the empty fields with "-"
+  ungroup() %>%
+  arrange(SO) %>%
+  pivot_wider(
+    names_from = SO, 
+    values_from = region_4,
+    values_fill = list("-"),
+    id_cols = band.of.ind.
+  )
+
+
+## test the above codes with small dataset, e.g. V86
+
+# V86_winter <- subset(BFS_all.UT.2, band.of.ind. == "V86") %>%
+#   mutate(time = as.Date(observation.date),
+#          region_4 = as.character(region_4)) %>%  # Convert region_4 to character
+#   mutate(SO = case_when(
+#     time>=as.Date("1994/12/1")&time<=as.Date("1995/2/28")~"1994W",	time>=as.Date("1995/6/1")&time<=as.Date("1995/8/31")~"1995S",
+#     time>=as.Date("1995/12/1")&time<=as.Date("1996/2/29")~"1995W",	time>=as.Date("1996/6/1")&time<=as.Date("1996/8/31")~"1996S",
+#     time>=as.Date("1996/12/1")&time<=as.Date("1997/2/28")~"1996W",	time>=as.Date("1997/6/1")&time<=as.Date("1997/8/31")~"1997S",
+#     time>=as.Date("1997/12/1")&time<=as.Date("1998/2/28")~"1997W",	time>=as.Date("1998/6/1")&time<=as.Date("1998/8/31")~"1998S",
+#     time>=as.Date("1998/12/1")&time<=as.Date("1999/2/28")~"1998W",	time>=as.Date("1999/6/1")&time<=as.Date("1999/8/31")~"1999S",
+#     time>=as.Date("1999/12/1")&time<=as.Date("2000/2/29")~"1999W",	time>=as.Date("2000/6/1")&time<=as.Date("2000/8/31")~"2000S",
+#     time>=as.Date("2000/12/1")&time<=as.Date("2001/2/28")~"2000W",	time>=as.Date("2001/6/1")&time<=as.Date("2001/8/31")~"2001S",
+#     time>=as.Date("2001/12/1")&time<=as.Date("2002/2/28")~"2001W",	time>=as.Date("2002/6/1")&time<=as.Date("2002/8/31")~"2002S",
+#     time>=as.Date("2002/12/1")&time<=as.Date("2003/2/28")~"2002W",	time>=as.Date("2003/6/1")&time<=as.Date("2003/8/31")~"2003S",
+#     time>=as.Date("2003/12/1")&time<=as.Date("2004/2/29")~"2003W",	time>=as.Date("2004/6/1")&time<=as.Date("2004/8/31")~"2004S",
+#     time>=as.Date("2004/12/1")&time<=as.Date("2005/2/28")~"2004W",	time>=as.Date("2005/6/1")&time<=as.Date("2005/8/31")~"2005S",
+#     time>=as.Date("2005/12/1")&time<=as.Date("2006/2/28")~"2005W",	time>=as.Date("2006/6/1")&time<=as.Date("2006/8/31")~"2006S",
+#     time>=as.Date("2006/12/1")&time<=as.Date("2007/2/28")~"2006W",	time>=as.Date("2007/6/1")&time<=as.Date("2007/8/31")~"2007S",
+#     time>=as.Date("2007/12/1")&time<=as.Date("2008/2/29")~"2007W",	time>=as.Date("2008/6/1")&time<=as.Date("2008/8/31")~"2008S",
+#     time>=as.Date("2008/12/1")&time<=as.Date("2009/2/28")~"2008W",	time>=as.Date("2009/6/1")&time<=as.Date("2009/8/31")~"2009S",
+#     time>=as.Date("2009/12/1")&time<=as.Date("2010/2/28")~"2009W",	time>=as.Date("2010/6/1")&time<=as.Date("2010/8/31")~"2010S",
+#     time>=as.Date("2010/12/1")&time<=as.Date("2011/2/28")~"2010W",	time>=as.Date("2011/6/1")&time<=as.Date("2011/8/31")~"2011S",
+#     time>=as.Date("2011/12/1")&time<=as.Date("2012/2/29")~"2011W",	time>=as.Date("2012/6/1")&time<=as.Date("2012/8/31")~"2012S",
+#     time>=as.Date("2012/12/1")&time<=as.Date("2013/2/28")~"2012W",	time>=as.Date("2013/6/1")&time<=as.Date("2013/8/31")~"2013S",
+#     time>=as.Date("2013/12/1")&time<=as.Date("2014/2/28")~"2013W",	time>=as.Date("2014/6/1")&time<=as.Date("2014/8/31")~"2014S",
+#     time>=as.Date("2014/12/1")&time<=as.Date("2015/2/28")~"2014W",	time>=as.Date("2015/6/1")&time<=as.Date("2015/8/31")~"2015S",
+#     time>=as.Date("2015/12/1")&time<=as.Date("2016/2/29")~"2015W",	time>=as.Date("2016/6/1")&time<=as.Date("2016/8/31")~"2016S",
+#     time>=as.Date("2016/12/1")&time<=as.Date("2017/2/28")~"2016W",	time>=as.Date("2017/6/1")&time<=as.Date("2017/8/31")~"2017S",
+#     time>=as.Date("2017/12/1")&time<=as.Date("2018/2/28")~"2017W",	time>=as.Date("2018/6/1")&time<=as.Date("2018/8/31")~"2018S",
+#     time>=as.Date("2018/12/1")&time<=as.Date("2019/2/28")~"2018W",	time>=as.Date("2019/6/1")&time<=as.Date("2019/8/31")~"2019S",
+#     time>=as.Date("2019/12/1")&time<=as.Date("2020/2/29")~"2019W",	time>=as.Date("2020/6/1")&time<=as.Date("2020/8/31")~"2020S",
+#     time>=as.Date("2020/12/1")&time<=as.Date("2021/2/28")~"2020W",	time>=as.Date("2021/6/1")&time<=as.Date("2021/8/31")~"2021S",
+#     time>=as.Date("2021/12/1")&time<=as.Date("2022/2/28")~"2021W",	time>=as.Date("2022/6/1")&time<=as.Date("2022/8/31")~"2022S",
+#     time>=as.Date("2022/12/1")&time<=as.Date("2023/2/28")~"2022W",	time>=as.Date("2023/6/1")&time<=as.Date("2023/8/31")~"2023S",
+#     TRUE ~ NA_character_
+#   )) %>%
+#   group_by(band.of.ind., SO) %>%
+#   summarize(
+#     regions = list(region_4),
+#     dates = list(time),
+#     .groups = 'drop'
+#   ) %>%
+#   mutate(region_4 = map2(regions, dates, ~{
+#     region_list <- .x
+#     date_list <- .y
+#     freq <- table(region_list)
+#     max_freq <- max(freq)
+#     tied_regions <- names(freq[freq == max_freq])
+#     
+#     if (length(tied_regions) > 1) {
+#       tied_dates <- date_list[region_list %in% tied_regions]
+#       latest_date <- max(tied_dates)
+#       latest_region_indices <- which(region_list %in% tied_regions & date_list == latest_date)
+#       latest_region <- region_list[latest_region_indices[1]]
+#       
+#       if (length(unique(date_list[latest_region_indices])) > 1) {
+#         return("Check")
+#       } else {
+#         return(latest_region)
+#       }
+#     } else {
+#       return(tied_regions)
+#     }
+#   })) %>%
+#   ungroup() %>%
+#   arrange(SO) %>%
+#   pivot_wider(
+#     names_from = SO, 
+#     values_from = region_4,
+#     values_fill = list("-"),
+#     id_cols = band.of.ind.
+#   )
+
+
+
+## Versions Comparison
+
+# subset_winter_new <- BFS_06_23_winter_new
+# subset_winter <- BFS_06_23_winter
+# 
+# identical(subset_winter_new, subset_winter)  
+# identical(dim(subset_winter_new), dim(subset_winter))   
+# identical(names(subset_winter_new), names(subset_winter))
+# identical(str(subset_winter_new), str(subset_winter)) 
+# 
+# 
+# differences <- bind_rows(
+#   winter = subset_winter, 
+#   winter_new = subset_winter_new, 
+#   .id = "dataset"
+# ) %>%
+#   group_by(across(-dataset)) %>%
+#   filter(n_distinct(dataset) != 1) %>%
+#   ungroup()
+# 
+# diff_winter_not_in_new2 <- anti_join(subset_winter, subset_winter_new)
+# diff_new2_not_in_winter <- anti_join(subset_winter_new, subset_winter)
+
+
+
+
 
 # remove the questionable region labels based on the 'check' results: E46 in 2015W, S43 in 2014W, S47 in 2014W
 
@@ -425,7 +605,7 @@ nrow(merged3)
 # remove individuals wintering in [N]orthern China and [O]ther regions, or with unclear wintering region
 merged4 = merged3[!(merged3$most_visited %in% c("N", "O", "unclear")), ]
 
-# number of individuals: 492
+# number of individuals: 495
 nrow(merged4)
 
 # set id as rownames
