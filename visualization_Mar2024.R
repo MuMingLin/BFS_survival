341391d43dd2aae4e7f349843ed9477e46f22807
MuMingLin <159245220+MuMingLin@users.noreply.github.com>
2024-05-08 14:09:52 +0800

    Hide lines not in use

--- visualization_Mar2024.R
+++ visualization_Mar2024.R
@@ -191,114 +191,114 @@ ggsave("p_1.svg", plot = p_1, width = 20, height = 15, units = "cm")
 
 
 
-### Model averaging estimates ####
-
-# read file of read estimates
-all.avg = read.csv("all.avg.0201.csv")
-
-# add age class column
-
-all.avg.age <- all.avg %>%
-  mutate(a02 = case_when(
-    agegroup == "0-1y" ~ "young(0-2)",
-    agegroup == "1-2y" ~ "young(0-2)",
-    agegroup == "2-3y" ~ "old(2+)",
-    agegroup == "3-4y" ~ "old(2+)",
-    agegroup == "4y+" ~ "old(2+)",
-    TRUE ~ "Other" # Default case
-  ))
-
-# create Phi dataframe
-Phi.est = all.avg.age[all.avg.age$parameter=="Phi",] 
-Phi.est.sar = Phi.est
-Phi.est.sar$sar = paste(Phi.est.sar$season, Phi.est.sar$a02, Phi.est.sar$region, sep = "-")
-# Phi plotting
-Phi <- ggplot(
-  Phi.est.sar, 
-  aes(x = sar, y = estimate, ymin = lcl, ymax = ucl)
-)
-# Vertical line with point in the middle
-Phi + geom_pointrange()
-
-# create p dataframe
-p.est = all.avg.age[all.avg.age$parameter=="p",] 
-p.est.sar = p.est
-p.est.sar$sar = paste(p.est.sar$season, p.est.sar$a02, p.est.sar$region, sep = "-")
-
-# p plotting
-P <- ggplot(
-  p.est.sar, 
-  aes(x = sar, y = estimate, ymin = lcl, ymax = ucl)
-)
-
-# Vertical line with point in the middle
-P + geom_pointrange()
-
-
-# get geometric means of whole time survivals (time effects not important)
-
-
-
-Phi.plotting.2 = 
-  Phi.plotting %>%
-  group_by(region, season, agegroup) %>%
-  mutate(
-    gm_estimate = exp(mean(log(estimate))),  # Calculate geometric mean
-    gm_se = exp(mean(log(replace(se, se ==0, 0.00001)))),       # How to calculate SE ???
-    gm_ucl = exp(mean(log(replace(ucl, ucl ==0, 0.00001)))),       # How to calculate???
-    gm_lcl = exp(mean(log(replace(lcl, lcl ==0, 0.00001))))       # How to calculate???
-  ) %>%
-  distinct(region, season, agegroup, .keep_all = TRUE)
-
-
-plot(Phi.plotting$season, Phi.plotting$estimate)
-plot(Phi.plotting$region, Phi.plotting$estimate)
-# plotting test (Y0-2:r*s)
-
-
-library(ggplot2)
-
-Phi.plotting.2$season_low =  as.numeric(as.factor(Phi.plotting.2$season)) - 0.1 
-Phi.plotting.2$season_high =  as.numeric(as.factor(Phi.plotting.2$season)) + 0.1 
-
-RXA.young = 
-  ggplot(data = Phi.plotting.2[Phi.plotting.2$agegroup=="0-1y"|Phi.plotting.2$agegroup=="1-2y",], 
-         aes(x = region, y = gm_estimate, color=`season`))+
-  geom_pointrange(aes(ymin = gm_estimate-gm_se, ymax = gm_estimate+gm_se)) + 
-  scale_color_manual(values = c("W-S" = "deepskyblue2", "S-W" = "lightsalmon")) +
-  geom_segment(aes(y = gm_lcl, yend = gm_lcl
-                   #, x = region_low, xend = region_high
-  ), linetype="solid", alpha=0.2, linewidth = 1) + 
-  geom_segment(aes(y = gm_ucl, yend = gm_ucl
-                   #, x = region_low, xend = region_high
-  ), linetype="solid", alpha=0.2, linewidth = 1) +
-  geom_pointrange(aes(ymin = gm_estimate-gm_se, ymax = gm_estimate+gm_se),position = position_dodge(width = 0)) + 
-  labs(title="BFS Seasonal Survival in Different Age Classes",x="Wintering Regions", y = "Survival Estimates (Φ)")+
-  theme_classic()+
-  theme(
-    legend.position=c(0.9, 0.2),
-    plot.title = element_text(hjust = 0.5, size = 16),
-  ) 
-
-
-
-ggplot(data = Phi.plotting.2[!Phi.plotting.2$region=="K" & Phi.plotting.2$agegroup=="3-4y"|Phi.plotting.2$agegroup=="4y+",], 
-       aes(x = region, y = gm_estimate, color=`season`))+
-  geom_pointrange(aes(ymin = gm_estimate-gm_se, ymax = gm_estimate+gm_se)) + 
-  scale_color_manual(values = c("W-S" = "deepskyblue2", "S-W" = "lightsalmon")) +
-  #  geom_segment(aes(y = gm_lcl, yend = gm_lcl
-  #                   , x = season_low, xend = season_high
-  #  ), linetype="solid", alpha=0.2, linewidth = 1) + 
-  # geom_segment(aes(y = gm_ucl, yend = gm_ucl
-  #                  , x = season_low, xend = season_high
-  # ), linetype="solid", alpha=0.2, linewidth = 1) +
-  geom_pointrange(aes(ymin = gm_estimate-gm_se, ymax = gm_estimate+gm_se),position = position_dodge(width = 0.1)) + 
-  labs(title="BFS Seasonal Survival in Different Age Classes",x="Wintering Regions", y = "Survival Estimates (Φ)")+
-  theme_classic()+
-  theme(
-    legend.position=c(0.9, 0.2),
-    plot.title = element_text(hjust = 0.5, size = 16),
-  ) +
-  ylim(0.7, 1)
+# ### Model averaging estimates ####
+# 
+# # read file of read estimates
+# all.avg = read.csv("all.avg.0201.csv")
+# 
+# # add age class column
+# 
+# all.avg.age <- all.avg %>%
+#   mutate(a02 = case_when(
+#     agegroup == "0-1y" ~ "young(0-2)",
+#     agegroup == "1-2y" ~ "young(0-2)",
+#     agegroup == "2-3y" ~ "old(2+)",
+#     agegroup == "3-4y" ~ "old(2+)",
+#     agegroup == "4y+" ~ "old(2+)",
+#     TRUE ~ "Other" # Default case
+#   ))
+# 
+# # create Phi dataframe
+# Phi.est = all.avg.age[all.avg.age$parameter=="Phi",] 
+# Phi.est.sar = Phi.est
+# Phi.est.sar$sar = paste(Phi.est.sar$season, Phi.est.sar$a02, Phi.est.sar$region, sep = "-")
+# # Phi plotting
+# Phi <- ggplot(
+#   Phi.est.sar, 
+#   aes(x = sar, y = estimate, ymin = lcl, ymax = ucl)
+# )
+# # Vertical line with point in the middle
+# Phi + geom_pointrange()
+# 
+# # create p dataframe
+# p.est = all.avg.age[all.avg.age$parameter=="p",] 
+# p.est.sar = p.est
+# p.est.sar$sar = paste(p.est.sar$season, p.est.sar$a02, p.est.sar$region, sep = "-")
+# 
+# # p plotting
+# P <- ggplot(
+#   p.est.sar, 
+#   aes(x = sar, y = estimate, ymin = lcl, ymax = ucl)
+# )
+# 
+# # Vertical line with point in the middle
+# P + geom_pointrange()
+# 
+# 
+# # get geometric means of whole time survivals (time effects not important)
+# 
+# 
+# 
+# Phi.plotting.2 = 
+#   Phi.plotting %>%
+#   group_by(region, season, agegroup) %>%
+#   mutate(
+#     gm_estimate = exp(mean(log(estimate))),  # Calculate geometric mean
+#     gm_se = exp(mean(log(replace(se, se ==0, 0.00001)))),       # How to calculate SE ???
+#     gm_ucl = exp(mean(log(replace(ucl, ucl ==0, 0.00001)))),       # How to calculate???
+#     gm_lcl = exp(mean(log(replace(lcl, lcl ==0, 0.00001))))       # How to calculate???
+#   ) %>%
+#   distinct(region, season, agegroup, .keep_all = TRUE)
+# 
+# 
+# plot(Phi.plotting$season, Phi.plotting$estimate)
+# plot(Phi.plotting$region, Phi.plotting$estimate)
+# # plotting test (Y0-2:r*s)
+# 
+# 
+# library(ggplot2)
+# 
+# Phi.plotting.2$season_low =  as.numeric(as.factor(Phi.plotting.2$season)) - 0.1 
+# Phi.plotting.2$season_high =  as.numeric(as.factor(Phi.plotting.2$season)) + 0.1 
+# 
+# RXA.young = 
+#   ggplot(data = Phi.plotting.2[Phi.plotting.2$agegroup=="0-1y"|Phi.plotting.2$agegroup=="1-2y",], 
+#          aes(x = region, y = gm_estimate, color=`season`))+
+#   geom_pointrange(aes(ymin = gm_estimate-gm_se, ymax = gm_estimate+gm_se)) + 
+#   scale_color_manual(values = c("W-S" = "deepskyblue2", "S-W" = "lightsalmon")) +
+#   geom_segment(aes(y = gm_lcl, yend = gm_lcl
+#                    #, x = region_low, xend = region_high
+#   ), linetype="solid", alpha=0.2, linewidth = 1) + 
+#   geom_segment(aes(y = gm_ucl, yend = gm_ucl
+#                    #, x = region_low, xend = region_high
+#   ), linetype="solid", alpha=0.2, linewidth = 1) +
+#   geom_pointrange(aes(ymin = gm_estimate-gm_se, ymax = gm_estimate+gm_se),position = position_dodge(width = 0)) + 
+#   labs(title="BFS Seasonal Survival in Different Age Classes",x="Wintering Regions", y = "Survival Estimates (Φ)")+
+#   theme_classic()+
+#   theme(
+#     legend.position=c(0.9, 0.2),
+#     plot.title = element_text(hjust = 0.5, size = 16),
+#   ) 
+# 
+# 
+# 
+# ggplot(data = Phi.plotting.2[!Phi.plotting.2$region=="K" & Phi.plotting.2$agegroup=="3-4y"|Phi.plotting.2$agegroup=="4y+",], 
+#        aes(x = region, y = gm_estimate, color=`season`))+
+#   geom_pointrange(aes(ymin = gm_estimate-gm_se, ymax = gm_estimate+gm_se)) + 
+#   scale_color_manual(values = c("W-S" = "deepskyblue2", "S-W" = "lightsalmon")) +
+#   #  geom_segment(aes(y = gm_lcl, yend = gm_lcl
+#   #                   , x = season_low, xend = season_high
+#   #  ), linetype="solid", alpha=0.2, linewidth = 1) + 
+#   # geom_segment(aes(y = gm_ucl, yend = gm_ucl
+#   #                  , x = season_low, xend = season_high
+#   # ), linetype="solid", alpha=0.2, linewidth = 1) +
+#   geom_pointrange(aes(ymin = gm_estimate-gm_se, ymax = gm_estimate+gm_se),position = position_dodge(width = 0.1)) + 
+#   labs(title="BFS Seasonal Survival in Different Age Classes",x="Wintering Regions", y = "Survival Estimates (Φ)")+
+#   theme_classic()+
+#   theme(
+#     legend.position=c(0.9, 0.2),
+#     plot.title = element_text(hjust = 0.5, size = 16),
+#   ) +
+#   ylim(0.7, 1)
 
 
